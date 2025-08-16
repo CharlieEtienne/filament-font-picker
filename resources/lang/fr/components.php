d66f96280d44e6f53093eb0ca1418c2f8c33273b
CHARLIE ETIENNE <ce@web-nancy.fr>
2025-08-16 19:02:53 +0200

    feat: add French translation

---
+++ resources/lang/fr/components.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'placeholder' => 'Sélectionnez une police',
+    'search_prompt' => 'Rechercher des polices',
+    'no_results' => [
+        'title' => 'Aucune police trouvée correspondant à',
+        'subtitle' => 'Essayez de rechercher une police serif, sans-serif, monospace ou display',
+    ],
+    'loading' => [
+        'fonts' => 'Chargement des polices Google...',
+        'preview' => 'Chargement de l\'aperçu...',
+    ],
+    'preview_text' => 'Portez ce vieux whisky au juge blond qui fume', //The quick brown fox jumps over the lazy dog
+];
